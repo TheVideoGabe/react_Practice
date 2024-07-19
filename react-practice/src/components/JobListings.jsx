97d08e5e8fac70e80eca8aa04e6149f77bb80770
gabrielcorralcardenas@gmail.com <gabrielcorralcardenas@gmail.com>
2024-07-18 22:51:11 -0700

    Added api proxying for Jason server, added the single page template, added params for job id, used data loaders & added the single page that displays the api endpoints data.

--- react-practice/src/components/JobListings.jsx
+++ react-practice/src/components/JobListings.jsx
@@ -1,5 +1,5 @@
 import { useState,useEffect } from 'react';
-import jobs from '../jobs.json'
+// import jobs from '../jobs.json'
 import JobListing from './JobListing'
 import Spinner from './Spinner';
 
@@ -9,7 +9,7 @@ const JobListings = ({isHome = false}) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs'
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs'
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
