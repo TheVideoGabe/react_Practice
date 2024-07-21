f7438904fdbadfca3da3a0b0111ad8e997596879
gabrielcorralcardenas@gmail.com <gabrielcorralcardenas@gmail.com>
2024-07-21 14:58:31 -0700

    Added an add job page, created a form within that add job page, added state and value variables to the form, passed functions as props to implement api calls, added delete crud functionality to remove jobs from jobs.json, used the react toastify package to visualize success on add or deleted jobs

--- Traversy_Media_2024/src/App.jsx
+++ Traversy_Media_2024/src/App.jsx
@@ -4,21 +4,42 @@ import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/JobPage';
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-  <Route index element={<HomePage/ >} />
-  <Route path='/jobs' element={<JobsPage />} />
-  <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-  <Route path='*' element={<NotFoundPage></NotFoundPage>} />
-  </Route>
-)
-);
+import AddJobPage from './pages/AddJobPage';
 
 const App = () => {
-  return <RouterProvider router={router} />;
-};
+  // add new job
+    const addJob = async (newJob) => {
+      const res = await fetch('/api/jobs', {
+        method:'POST',
+        headers: {
+          'content-Type': 'application/json'
+        },
+        body: JSON.stringify(newJob),
+      })
+      return
+    }
+
+//delete job
+      const deleteJob = async (id) => {
+        const res = await fetch(`/api/jobs/${id}`, {
+          method:'DELETE',
+        })
+        return;
+      }
+
+    const router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage/ >} />
+    <Route path='/jobs' element={<JobsPage />}/>
+    <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />}/>
+    <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+    <Route path='*' element={<NotFoundPage></NotFoundPage>} />
+    </Route>
+  )
+);
+
+return <RouterProvider router={router} />;
+}
 
 export default App;
--- Traversy_Media_2024/src/pages/JobPage.jsx
+++ Traversy_Media_2024/src/pages/JobPage.jsx
@@ -1,6 +1,7 @@
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 // import { useState, useEffect } from "react"
 // import Spinner from "../components/Spinner";
 
@@ -26,9 +27,22 @@ import { Link } from "react-router-dom";
 //     return loading ? <Spinner /> : <h1>{job.title}</h1>
 // }
 
-const jobPage = () => {
+const jobPage = ({deleteJob}) => {
+    const navigate = useNavigate();
     const {id} = useParams();
     const job = useLoaderData();
+
+    const onDeleteClick = (jobId) => {
+        const confirm = window.confirm('Are you sure you want to delete this?')
+
+        if(!confirm) return;
+
+        deleteJob(jobId)
+
+      toast.success('Job Deleted Successfully');
+
+        navigate('/jobs')
+    };
 
     return (
     <>
@@ -110,7 +124,7 @@ const jobPage = () => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job
                 </Link>
-              <button
+              <button onClick={() => onDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
